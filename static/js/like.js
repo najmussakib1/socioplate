262f56e81b46dcb5aa69a03d15de8ead4714bf02
Md. Najmus Sakib <89391621+najmussakib1@users.noreply.github.com>
2021-09-18 17:33:01 +0600

    Add files via upload

--- static/js/like.js
+++ static/js/like.js
@@ -1,16 +1,12 @@
 function likefunc(sno){
     let endpoint = 'http://127.0.0.1:8000/'+'likepost/'+ String(sno);
-    let xhr = new  XMLHttpRequest();
-    xhr.open('GET',endpoint,true);
-    xhr.onload = function(){
-        if (this.status===200){
-            a = JSON.parse(this.response)
-            let buttn = document.getElementById(String(sno));
-            buttn.innerHTML = `<span></span>
-    <span></span>
-    <span></span>
-    <span></span>`+'Like: '+String(a["nooflikes"])
-        }
-    }
-    xhr.send()
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        let buttn = document.getElementById(String(sno));
+        buttn.innerHTML = `<span></span>
+        <span></span>
+        <span></span>
+        <span></span>`+'Like: '+String(data["nooflikes"]);
+    });
 }
